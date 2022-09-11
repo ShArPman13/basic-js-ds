1c23445216ae514d1f11b259adda600b4ff1518f
sharpman13 <sharpman2001@gmail.com>
2022-09-11 22:50:43 +0200

    feat: complited all tasks

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,134 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null
+  }
+}
+
 class BinarySearchTree {
+  constructor() {
+    this.myroot = null
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.myroot ? this.myroot : null
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.myroot = insertNode(this.myroot, data);
+
+    function insertNode(node, data) {
+      if (!node) {
+        return new Node(data)
+      }
+      if (node.data === data) {
+        return node
+      }
+      if (data < node.data) {
+        node.left = insertNode(node.left, data)
+      } else {
+        node.right = insertNode(node.right, data)
+      }
+      return node
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchWithIn(this.myroot, data);
+
+    function searchWithIn(node, data) {
+      if (!node) {
+        return false
+      }
+
+      if (node.data === data) {
+        return true
+      }
+
+      return node.data > data ? searchWithIn(node.left, data) : searchWithIn(node.right, data)
+
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return findData(this.myroot, data)
+
+    function findData(node, data) {
+      if (!node) {
+        return node
+      }
+      if (node.data === data) {
+        return node
+      }
+      return node.data < data ? findData(node.right, data) : findData(node.left, data)
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.myroot = removeNode(this.myroot, data);
+
+    function removeNode(node, data) {
+      if (!node) {
+        return null
+      }
+      if (data < node.data) {
+        node.left = removeNode(node.left, data)
+        return node;
+      } else if (node.data < data) {
+        node.right = removeNode(node.right, data)
+        return node
+      } else {
+        if (!node.left && !node.right) {
+          return null
+        }
+
+        if (!node.left) {
+          node = node.right
+          return node
+        }
+
+        if (!node.right) {
+          node = node.left
+          return node
+        }
+
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+        node.data = minFromRight.data;
+        node.right = removeNode(node.right, minFromRight.data)
+        return node
+      }
+    }
   }
+
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.myroot) {
+      return 'no elements in tree'
+    }
+
+    let node = this.myroot;
+    while (node.left) {
+      node = node.left
+    }
+    return node.data
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.myroot) {
+      return 'no elements in tree'
+    }
+    let node = this.myroot;
+    while (node.right) {
+      node = node.right
+    }
+    return node.data
   }
 }
 
--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
+
 class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head
+  }
+
+  enqueue(value) {
+    if (this.length === 0) {
+      this.head = new ListNode(value)
+    } else {
+      let current = this.head
+      while (current.next) {
+        current = current.next
+      }
+      current.next = new ListNode(value)
+    }
+    this.length++
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    let last = this.head;
+    this.head = this.head.next;
+
+    return last.value
   }
 }
 
